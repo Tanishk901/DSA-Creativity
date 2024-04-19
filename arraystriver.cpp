a76d477b62728e51f1f3f72b6540482a6f951337
Tanishk Mahakalkar <tanishk901@gmail.com>
2024-04-19 10:15:26 +0530

    Add files via upload

---
+++ arraystriver.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    //2D array
+
+    // int arr[3][5];
+
+    // arr[1][3] = 78;
+    // cout << arr[1][2];
+    // return 0;
+
+    string s = "Tanishk";
+    int len = s.size();
+    s[len-1] = 'z';
+    cout << s[len - 1] << endl << len;
+    return 0;
+
+    
+}
