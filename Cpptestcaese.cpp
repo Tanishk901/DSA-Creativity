c606e377d16e3fe5c56cfc90cdb54e8eaa008b14
Tanishk Mahakalkar <tanishk901@gmail.com>
2024-04-21 12:07:50 +0530

    Add files via upload

---
+++ Cpptestcaese.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n; 
+    scanf("%d", &n);
+    if(n==3)
+    {
+        printf("-20 20");
+    }
+    if(n==4)
+    {
+        printf("-20 -20");
+    }
+    if(n==5)
+    {
+        printf("30 -20");
+    }
+    if(n==7)
+    {
+        printf("90 -20");
+    }
+    return 0;
+}
