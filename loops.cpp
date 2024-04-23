368589e83a4814187a91bd6bf18cfbc68c79bf56
Tanishk Mahakalkar <tanishk901@gmail.com>
2024-04-24 02:10:36 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int i;
+    // for( i = 1; i <= 25; i = i + 5) {
+    //     cout << "Tanishk " << i << endl;
+    // }
+    // cout << i << endl;
+
+
+    int i = 45;
+    do {
+        cout << "Tanishk "<< i << endl;
+        i = i + 25;
+    } while (i<=10);
+    cout << i << endl;
+   
+    return 0;
+}
