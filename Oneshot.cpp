7ae728b8f46419a2672db7f8733c6a35a959525e
Tanishk Mahakalkar <tanishk901@gmail.com>
2024-04-25 09:09:09 +0530

    Add files via upload

---
+++ Oneshot.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+
+
+// int main() {
+//     cout << "Hey Sigma!" << endl << "Hey Tanni!" << endl;
+//     cout << "Hey Kratos!";
+//     return 0;
+// }
+
+// int main() {
+//     int x, y;
+//     cin >> x >> y;
+//     cout << "Value of x: " << x << " and y: " << y;
+    
+//     return 0;
+
+
+int main() {
+    // int 
+    // int x = 10;
+    // //long 
+    // long y = 150;
+    // // long long
+    // long long z = 15000000;
+
+    //float,  double
+    // float x = 5.6;
+    // float y = 12;
+    // cout<< "The value of y: " << y;
+
+    // string s1;
+    // string s2;
+    // cin >> s1 >> s2;
+    // cout << s1 << " " << s2;
+
+//     string str;
+//     //getline - takes the entire line
+//     getline(cin, str); //getline - takes the entire line
+//     cout << str;
+
+
+    // char ch;
+    // cin >> ch;
+    // cout << ch;
+    
+    // char ch = 'g';
+    // cout << ch;
+
+    //wap tht takes an i/p of age and prints if you are adult or not
+
+    // int age;
+    // cin >> age;
+    // if(age>=18) {
+    //     cout << "You are an Adult!" << endl;
+    // }
+    // else if(age<10){
+    //     cout << "You're not an Adult" << endl;
+    // }
+
+    // int marks;
+    // cin >> marks;
+    // if(marks < 25) {
+    //     cout << "F";
+    // }
+    // else if(marks <= 44 ){
+    //     cout << "E";
+    // }
+    // else if(marks <= 49 ){
+    //     cout << "D";
+    // }
+    // else if(marks <= 59 ){
+    //     cout << "C";
+    // }
+    // else if(marks <= 79 ){
+    //     cout << "B";
+    // }
+    // else if(marks <= 100 ){
+    //     cout << "A";
+    // }
+
+    // int age;
+    // cin >> age;
+    // if(age < 18) {
+    //     cout << "Not eligible for job!";
+    // }
+    // else if(age <= 54) {
+    //     cout << "eligible for job";
+    // }
+    // else if (age <= 57) {
+    //     cout << "eligible for job"; 
+    //     //nested if
+    //     if(age >= 55){
+    //         cout << ", but retirement soon!";
+    //     }
+    // }
+
+    // else {
+    //     cout << "retirement time!";
+    // }
+    // return 0;
+
+
+    // int day;
+    // cin >> day;
+
+    // switch (day)
+    // {
+    // case 1:
+    //     cout << "Monday";
+    //     break;
+    // case 2:
+    //     cout << "Tues";
+    //     break;
+    // case 3:
+    //     cout << "Wed";
+    //     break;
+    // case 4:
+    //     cout << "Thursday";
+    //     break;
+    // case 5:
+    //     cout << "Fri";
+    //     break;
+    // case 6:
+    //     cout << "Sat";
+    //     break;
+    // case 7:
+    //     cout << "Sun";
+    //     break;
+    // default:
+    //     cout << "Invalid!";
+    //     break;
+    // cout << "Invalid Check!";
+
+   
+
+       
+    
+    // }
+    // cout <<" Check";
+
+    
+
+    return 0;
+
+
+}
